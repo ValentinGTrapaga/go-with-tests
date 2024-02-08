66e7d8862e27c4240fa30db4920d8ba265a45d1f
ValentinGTrapaga <valentingt22@gmail.com>
2024-02-08 01:27:22 -0300

    add language options and switch statements

--- hello-world/main.go
+++ hello-world/main.go
@@ -4,15 +4,36 @@ import (
 	"fmt"
 )
 
-const englishHelloPrefix = "Hello, "
+const (
+	french  = "French"
+	spanish = "Spanish"
 
-func Hello(name string) string {
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHeloPrefix   = "Bonjour, "
+)
+
+func Hello(name, language string) string {
 	if name == "" {
-		return englishHelloPrefix + "World"
+		name = "World"
 	}
-	return englishHelloPrefix + name
+	return getPrefixByLanguage(language) + name
+
+}
+
+// prefix is a named return value, defaults to the zero value, in this case ""
+func getPrefixByLanguage(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHeloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return prefix
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", "Spanish"))
 }
